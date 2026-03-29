51b8eda502d11000d4c97bfb70b044ab338319ac
KhongPhaiNhen <daoxuanthinh.6886@gmail.com>
2026-03-29 22:30:39 +0700

    Something i forgot to push 5 days ago

--- Assignment/_list_q.py
+++
@@ -1,8 +0,0 @@
-import json
-for f in ['questions_a','questions_b','questions_c','questions_d']:
-    data = json.load(open(f+'.json',encoding='utf-8'))
-    for ch in data['chapters']:
-        n = ch['number']
-        for diff in ['easy','medium','hard']:
-            for i,q in enumerate(ch['questions'][diff]):
-                print(f"CH{n:02d} {diff[0].upper()}{i+1}: {q['q'][:90]}")
